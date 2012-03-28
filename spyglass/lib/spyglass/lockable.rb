2fe539ecd7e84b96cc4cdddf7fedc1576dc21e34
Jonathan Calvert <jcalvert@vetstreet.com>
2012-03-28 11:16:06 -0400

    Add lockable

---
+++ spyglass/lib/spyglass/lockable.rb
@@ -0,0 +1,21 @@
+module Spyglass
+  module Lockable
+    def is_locked?
+      !@redis.get('sync.lock').nil?
+    end
+
+    def acquire_lock?
+      if @redis.setnx 'sync.lock', Time.now.to_i+1001
+        true
+      else
+        if Time.now.to_i < @redis.get('sync.lock').to_i #lock unexpired
+          return false
+        else
+          lock = @redis.getset 'sync.lock', Time.now.to_i+1001
+          return Time.now.to_i > lock.to_i
+        end
+      end
+    end
+
+  end
+end
